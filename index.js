b5df170d9301f5c481ab98e0a17043f8b947a698
Safdar Alee <>
2023-03-15 12:51:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+/*/Q1=DataTypes in Javascript given below
+//Numbers,Strings,Boolean,Unddefined,Null,Object
+
+
+//Q2= Differentiating Null and undefined 
+//undefined can be written like this;
+let a = {}
+console.log(a)
+//Null can written like this ;
+let b = "Null";
+console.log(b)
+
+
+
+//Q3=Rules to name the Variables in  javascript
+i=Var name can not be started with a number like
+var 1name=
+var my name= space is not allowed 
+
+var name1= is allowed
+var $name= is allowed
+var my_name= is also allowed
+var myname= is allowed too 
+Everyone must follow these rules
+
+
+
+
+//Q4=Implicit & Explicit
+Explicit is like where u have to tell the machine that what are u going to get executed, Suppose
+int myvar= 30
+Famous languages work under this stretegy are C++ and Java
+
+//Implicit
+Implicit is like where u dont need to tell the machine that what is going to be executed , only putting of the values would be enough for the machine to get it understand like 
+Javascript,php and python
+
+
+
+
+
+
+
+//Q5= Strongly typed lanuguages and loosely typed languages
+C++ and Java are strongly typed languages 
+Javascript and Phython are looslely typed languages 
+//Q6=A SOurce code and IDE
+
+
+
+//Q7=Differentiating IDE and Source code
+Text editors tend to be simpler than IDEs but the best text editors have a slew of features built-in, which can also be extended to add support for other languages. In addition, they can execute code without even leaving the application. Plus, some text editors provide syntax highlighting, version control, but built-in debugging is still a long shot even for best text editors out there. IDEs, on the other hand, are undoubtedly more feature-rich with all the bells and whistles, including code intelligence, project management, debugging, compiling, and more.
+
+
+
+//Q8=Environment setup in programming 
+
+
+
+
+//Q9=Why is VS code a hybrid of a Source editor and IDE
+
+
+
+//Q10= Static and Dynamic types with Ex
+Javascript is a Dynamic language because it changes itself normally while others are static
+
+
+
+//Q11=Why objects are called Kings in Javascript
+Objects can store many values or even a difficult values in it so that is why javascript is heavily depandant on it
+
+
+//Q12=Case sensitive(java,javascript,c++) and case Insensitive(Php)
+Case sensitive is like if u mentioned any variable with capital or small letters that must be mentioned while consoling it , exp
+var myname="Safdar"
+console.log(myName) it will through an error and it it called case sensitivity
+$myvar = "Safdar"
+print($Myvar)
+print($myvar)
+print($MYVAR) these are valid in php and are examples of case insenstivitity
+
+
+
+//Q13=Describe Variables and a List of Variable types 
+Variables can be defined in three types 
+Let Var and Const*/
